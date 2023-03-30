05fa8fa0270ee99173b63fb9b6ea91594c85ec17
DevBritto <davidbrittoo99@gmail.com>
2023-03-30 15:58:47 -0300

    first commit

---
+++ src/components/tittle/index.js
@@ -0,0 +1,10 @@
+import { H1 } from './../../containers/pedidos'
+
+function Tittle (props) {
+    console.log(props)
+    return <H1>{props.children}</H1>
+
+
+} 
+export default Tittle
+
---
+++ src/containers/index.js
@@ -0,0 +1,7 @@
+import { H1 } from "./styles"
+import H1 from '../components/tittle'
+function Container() {
+
+    return <H1>Testando as Props</H1>
+}
+export default Container
